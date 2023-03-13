bb39278899c4e1e6d7085815e5b8ea8d2c86f4ad
zhangxu <zhangxu@jianguodata.com>
2023-03-13 19:32:02 +0800

    实现MainActivity的大体

--- app/src/main/java/com/example/android_study/adapter/FixedFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android_study/adapter/FixedFragmentPagerAdapter.java
@@ -5,21 +5,31 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-public class FixedFragmentPagerAdapter  extends FragmentPagerAdapter {
+public class FixedFragmentPagerAdapter extends FragmentPagerAdapter {
     private Fragment[] mFragments = null;
     private String[] mTitles = null;
 
     public FixedFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
-
     }
 
+    /**
+     * 返回每一页指定位置的Fragment对象
+     *
+     * @param position
+     * @return
+     */
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return  mFragments[position];
+        return mFragments[position];
     }
 
+    /**
+     * 返回有多少个Fragment
+     *
+     * @return
+     */
     @Override
     public int getCount() {
         return mFragments == null ? 0 : mFragments.length;
